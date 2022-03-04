959c3344b06bdda89bf50d766cf920cce0b69c4a
João Pedro Bruno Machado <CIANDT\joaomachado@lnb035465cps.ciandt.global>
2022-03-04 12:01:20 -0300

    Creating class 'Funcionario'

---
+++ src/TesteFuncionario.java
@@ -0,0 +1,14 @@
+public class TesteFuncionario {
+    public static void main(String[] args) {
+
+        Funcionario nico = new Funcionario();
+
+        nico.setName("Nico Steppat");
+        nico.setCpf("11111111111");
+        nico.setSalario(2600.00);
+
+        System.out.println(nico.getName());
+        System.out.println(nico.getBonification());
+    }
+
+}
